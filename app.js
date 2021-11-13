2652878888e7fcfea8ea9aaf2bc9a1bf047c483e
Vikramjit Singh <vikramjitsingh551@gmail.com>
2021-11-12 22:38:03 -0700

    added HTTPS

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require("express");
 var app = express();
 
+var https = require("https");
+var fs = require("fs")
+
 var embeddedURLs = require("./json/embeddedURLs.json");
 var navbarItems = require("./json/navbarItems.json");
 
 var mediaUploadRoutes = require("./routes/mediaupload");
 
-var port = 80;
+var http_port = 80;
+var https_port = 443;
+var https_options = { key: fs.readFileSync('./cert/server.key'), cert: fs.readFileSync('./cert/server.cert')};
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"))
+
+
 
 app.get("/", (req, res) => {
     res.render("home.ejs", {navbarItems: navbarItems});
@@ -25,6 +32,17 @@ app.get("/documentation", (req, res) => {
 
 app.use("/mediaupload", mediaUploadRoutes);
 
-app.listen(process.env.PORT||port,process.env.IP,()=>{
-    console.log("Server started on port "+port);
+app.use((req, res, next) => {
+    if (!req.secure) {
+       return res.redirect("https://" + req.headers.host + req.url);
+    }
+    next();
+});
+
+app.listen(process.env.PORT||http_port,process.env.IP,()=>{
+    console.log("Server started on port "+http_port);
+});
+
+https.createServer(https_options, app).listen(https_port, () => {
+    console.log("Server started on port "+https_port);
 });
